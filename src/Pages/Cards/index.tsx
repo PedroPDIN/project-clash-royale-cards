53d70898e75f8cde1632463a49802162c713beeb
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-22 17:12:48 -0300

    adiciona tentativa de requisição

--- src/Pages/Cards/index.tsx
+++ src/Pages/Cards/index.tsx
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Components from '../../Components';
+import getAllCards from '../../Services/api/GetAllCards';
+import ICards from '../../Interfaces/Cards';
 
 import SMain from './styled';
 
 const Cards = () => {
   const [active, setActive] = useState<boolean>(false);
+  const [cards, setCards] = useState<ICards[] | []>([]);
+
+  useEffect(() => {
+    getAllCards().then((infoCards) => setCards(infoCards as any));
+  }, [cards]);
+
+  console.log(cards);
 
   return (
     <SMain>
