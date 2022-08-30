d31dc67c89fbb3e2f32a0d4eff201c64e8acf3db
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-29 22:53:40 -0300

    fix: alteração da função auxiliar de redirecionamento de detalhes do card.

--- src/Utils/helpers/nameCardLink.tsx
+++ src/Utils/helpers/nameCardLink.tsx
@@ -2,6 +2,11 @@ const nameCardLink = (name: string): any => {
   const currentName = name.toLowerCase();
   const currentArrayName = currentName.split('');
 
+  if (currentArrayName.includes('.') && currentArrayName.includes(' ')) {
+    const valueCurrent = currentName.split('.').join('');
+    return valueCurrent.split(' ').join('-');
+  }
+
   if (currentArrayName.includes('.')) {
     return currentName.split('.').join('');
   }
